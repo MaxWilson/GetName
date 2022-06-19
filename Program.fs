9e17ce54abb2fec411ad403279e2c32f7bb50789
Max Wilson <maxw@microsoft.com>
2022-06-18 17:09:39 -0700

    Initial

---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+
+let main args =
+    let getName path =
+        try
+            let path = System.IO.Path.GetFullPath path
+            let asm = System.Reflection.Assembly.LoadFrom path
+            asm.FullName
+        with _ -> $"Invalid path: '{path}'"
+    match args with
+    | [] -> printfn "Usage: getName <pathsToDlls>"
+    | ["read"] ->
+        let rec loop() =
+            let line = Console.ReadLine()
+            if line.Length > 0 then
+                printfn $"::{line|>getName}"
+                loop()
+        loop()
+    | paths ->
+        for path in paths do
+            printfn $"{getName path}"
